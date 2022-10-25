5edc2fdb82a6efe33caa052846484cd29f63f1ed
jmargal <jmargal25@gmail.com>
2022-10-25 21:22:09 +0200

    Metodos

--- controllers/cervezas.js
+++ controllers/cervezas.js
@@ -1,23 +1,34 @@
-const db = require('../models/db')
+const db = require("../models/db");
+db.connect('./data');
+db.loadCollections['cervezas'];
+db.loadCollections['bares']
 
-function getBeers(req, res){
-    res.json(db.cervezas.find())
+const arrayCervezas=db.cervezas.find();
+
+function getBeers(req, res) {
+  res.json(arrayCervezas);
 }
 
-function getBeer(req, res){
-
+function getBeer(req, res) {
+  let idCer = req.params.id;
+  for (let i = 0; i < arrayCervezas.length; i++) {
+    if ((arrayCervezas[i].id ===idCer)) {
+      res.json(arrayCervezas[i]);
+    }
+  }
 }
 
-function addBeer(req, res){
-
+function addBeer(req, res) {
+  arrayCervezas.push(req.body);
+  res.json({ mensaje: "post hecho" });
 }
 
-function deleteBeer(req, res){
-
+function deleteBeer(req, res) {
+  let id = req.params.id;
+  arrayCervezas.splice(id - 1, 1);
+  res.json({ mensaje: "delete hecho" });
 }
 
-function editBeer(req, res){
+function editBeer(req, res) {}
 
-}
-
-module.exports = { getBeers, getBeer, addBeer, deleteBeer, editBeer}
+module.exports = { getBeers, getBeer, addBeer, deleteBeer, editBeer };
